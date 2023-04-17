97cf10d49dc24918eab8fb7aabdf30ea6ba1353a
mateus <91036156+mateusfcardozo@users.noreply.github.com>
2023-04-17 19:35:27 -0300

    Update banco.java

--- banco.java
+++ banco.java
@@ -1,19 +1,23 @@
-/*A função banco possui um método main, que inicia a execução do programa, e outras três funções: cadastrarCliente, realizarLogin e atualizarCliente.
+/*
+A função "banco(linha 20 a 160)" possui um método main, que inicia a execução do programa, e outras três funções: cadastrarCliente, realizarLogin e atualizarCliente.
 
-A função "cadastrarCliente" permite cadastrar um novo cliente no sistema bancário. Ele solicita ao usuário que informe o nome, CPF, senha e celular do cliente. Se o número de clientes cadastrados ultrapassar o limite de 10,
+A função "cadastrarCliente(linha 25 a 46)" permite cadastrar um novo cliente no sistema bancário. Ele solicita ao usuário que informe o nome, CPF, senha e celular do cliente. Se o número de clientes cadastrados ultrapassar o limite de 10,
 a função exibe uma mensagem informando que o limite foi atingido. Caso contrário, os dados do novo cliente são armazenados em uma matriz clientes.
 
-A função "realizarLogin" permite que um cliente faça login no sistema. Ele solicita que o usuário informe seu CPF e senha, e verifica se há um cliente com esses dados cadastrado no sistema. Se houver, a função exibe o nome,
+A função "realizarLogin(linha 48 a 65)" permite que um cliente faça login no sistema. Ele solicita que o usuário informe seu CPF e senha, e verifica se há um cliente com esses dados cadastrado no sistema. Se houver, a função exibe o nome,
 saldo e celular do cliente e chama a função atualizarCliente para permitir que o cliente atualize seus dados. Se não houver, a função exibe uma mensagem informando que o CPF ou a senha estão incorretos.
 
-A função "atualizarCliente" permite que um cliente atualize seus dados cadastrados no sistema. Ele exibe um menu com as opções disponíveis: atualizar nome, atualizar senha, atualizar celular, adicionar saldo e voltar.
+A função "atualizarCliente(linha 67 a 130" permite que um cliente atualize seus dados cadastrados no sistema. Ele exibe um menu com as opções disponíveis: atualizar nome, atualizar senha, atualizar celular, adicionar saldo e voltar.
 Cada opção chama um método específico que realiza a atualização correspondente. O método de adicionar saldo também atualiza o saldo do cliente na matriz clientes.
+
+A função "main(linha 132 a 159)" serve para o usuario escolher umas das opções e ira mandar para uma das funções.
 
 Em geral, o programa permite que novos clientes se cadastrem, que clientes existentes façam login e atualizem seus dados cadastrados, e que o saldo dos clientes seja atualizado.
 Ele armazena os dados dos clientes em uma matriz e utiliza a classe Scanner para receber entradas do usuário.
 */
 import java.util.Scanner;
-public class banco{
+
+public class Banco{
     static Scanner scanner = new Scanner(System.in);
     static String[][] clientes = new String[10][5]; 
     static int auxiliar = 0;
@@ -67,7 +71,8 @@ public class banco{
         System.out.println("2 - Senha");
         System.out.println("3 - Celular");
         System.out.println("4 - Adicionar saldo");
-        System.out.println("5 - Voltar");
+        System.out.println("5 - Adicionar saldo");
+        System.out.println("6 - Voltar");
         System.out.print("Opção: ");
         opcao = scanner.nextInt();
         scanner.nextLine();
@@ -101,6 +106,21 @@ public class banco{
                 atualizarCliente(auxiliarCliente);
                 break;
             case 5:
+                System.out.print("Valor a retirar: R$");
+                String valorRetirar = scanner.nextLine();
+                double saldoRetirar = Double.parseDouble(clientes[auxiliarCliente][4]);
+                double novoSaldoRetirada = saldoRetirar - Double.parseDouble(valorRetirar);
+                clientes[auxiliarCliente][4] = String.format("%.2f", novoSaldoRetirada);
+                if(novoSaldoRetirada < 0){
+                    System.out.println("Saldo Insuficiente.");
+                }
+                else{
+                    clientes[auxiliarCliente][4] = String.format("%.2f",novoSaldoRetirada);
+                    System.out.println("Saldo atualizado.");
+                }
+                atualizarCliente(auxiliarCliente);
+                break;
+            case 6:
                 System.out.println("Voltando...");
                 break;
             default:
@@ -113,7 +133,7 @@ public class banco{
         int opcao;
         do {
             System.out.println("\nbanco Turus");
-            System.out.println("\nEscolha uma das opções");
+            System.out.println("Escolha uma das opções");
             System.out.println("1 - Cadastrar-se");
             System.out.println("2 - Fazer login");
             System.out.println("3 - Sair");
