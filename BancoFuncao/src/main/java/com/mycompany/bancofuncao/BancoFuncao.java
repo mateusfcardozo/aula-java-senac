f1fc5eeed0d8652f3ac0e792768104d338ce481a
mateus <91036156+mateusfcardozo@users.noreply.github.com>
2023-04-26 19:57:46 -0300

    Add files via upload

---
+++ BancoFuncao/src/main/java/com/mycompany/bancofuncao/BancoFuncao.java
@@ -0,0 +1,16 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bancofuncao;
+
+/**
+ *
+ * @author mateus
+ */
+public class BancoFuncao {
+
+    public static void main(String[] args) {
+        
+    }
+}
